203a56026167c5d801a664baa155850c27dbecf3
Juan José Salazar García <jjslzgc@gmail.com>
2014-09-19 14:36:58 +0200

    Modified README and added LICENSE file and woody.py that includes main function

---
+++ README.md
@@ -0,0 +1,12 @@
+Woody 0.0.1
+
+	Copyright (c) 2014 Juan José Salazar García - jjslzgc@gmail.com
+
+	- Description
+
+		An simple and lightweight micro-blogging client
+
+	- License
+
+		Woody is licensed under the GNU General Public License version 3, you should read LICENSE file to get more details about this
+
---
+++ woody.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  This file is part of Woody.
+#
+#  Copyright (c) 2014 Juan Jose Salazar Garcia jjslzgc@gmail.com
+#
+#  Woody is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Woody is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Woody.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+def main():
+	pass
+
+if __name__ == '__main__': main()
+
