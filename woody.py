618c9fab68a5f39f0befbb23202be849ae5f7d8c
Juan José Salazar García <jjslzgc@gmail.com>
2015-07-20 12:04:26 +0200

    Removed PumpController and any reference to it on woody.py

--- woody.py
+++ woody.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 #
@@ -21,8 +21,7 @@
 #
 
 from model.account import OAuthAccount
-#from net.twittercontroller import TwitterController
-from net.pumpcontroller import PumpController
+from net.twittercontroller import TwitterController
 
 def register(url = None):
 	if url:
@@ -30,27 +29,24 @@ def register(url = None):
 	return raw_input('Verifier:')
 
 def main():
-	#twitterAccount = OAuthAccount('Twitter', 'Test account')
-	#twitterController = TwitterController(twitterAccount, register)
-	#
-	#print 'Access Token: ', twitterAccount.key
-	#print 'Access Token Secret: ', twitterAccount.secret
-	#
-	#print '### Timeline (10 last tweets) ###'
-	#for tweet in twitterController.timeline(10):
-	#	print tweet.text
-	#
-	#print '### Followers ###'
-	#for follower in twitterController.followers():
-	#	print follower.screen_name
-	#
-	#print '### Following ###'
-	#for following in twitterController.following():
-	#	print following.screen_name
-	pumpAccount = OAuthAccount('Pump.io', 'woodytester@microca.st')
-	pumpController = PumpController(pumpAccount, register)
-	print(pumpController.timeline(10))
-
+	twitterAccount = OAuthAccount('Twitter', 'Test account')
+	twitterController = TwitterController(twitterAccount, register)
+	
+	print 'Access Token: ', twitterAccount.key
+	print 'Access Token Secret: ', twitterAccount.secret
+	
+	print '### Timeline (10 last tweets) ###'
+	for tweet in twitterController.timeline(10):
+		print tweet.text
+	
+	print '### Followers ###'
+	for follower in twitterController.followers():
+		print follower.screen_name
+	
+	print '### Following ###'
+	for following in twitterController.following():
+		print following.screen_name
+	
 
 if __name__ == '__main__':
 	main()
