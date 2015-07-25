490481ea895f9683f0475d1c61135248af432439
Juan Jose Salazar Garcia <jjslzgc@gmail.com>
2015-07-25 20:28:55 +0200

    Created class skeleton to account management

---
+++ model/accountmanager.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+#
+#  This file is part of Woody.
+#
+#  Copyright (c) 2014-2015 Juan Jose Salazar Garcia jjslzgc@gmail.com
+#
+#  Woody is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Woody is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Woody.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from account import Account
+
+class AccountManager(object):
+	def create(self, account):
+		pass
+
+	
+	def modify(self, account):
+		pass
+
+
+	def remove(self, account):
+		pass
+
+	
+	def get(self, name):
+		pass
+
+
+	def getAllByType(self, type):
+		pass
+
+
+	def getAll(self):
+		pass
