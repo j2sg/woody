2c48f68e8c5d7c93b12da6505c80a68858b68ba6
Juan Jose Salazar Garcia <jjslzgc@gmail.com>
2015-08-07 18:22:28 +0200

    Removed class Networks && Moved atribute supported to class Account. Created class methods getAccount, supportedMethods, hasSupport, networkToAccount and accountToNetworks

--- model/account.py
+++ model/account.py
@@ -19,13 +19,62 @@
 #  along with Woody.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-class Networks(object):
-    supported = ['Twitter']
-
 class Account(object):
+    supported = {'OAuth'    : ['twitter'],
+                 'UserPass' : []}
+
+
     def __init__(self, network, name):
         self.network = network
         self.name = name
+
+
+    @classmethod
+    def getAccount(cls, network, name):
+        account = cls.networkToAccount(network)
+
+        if account == 'OAuth':
+            return OAuthAccount(network, name)
+        elif account == 'UserPass':
+            return UserPassAccount(network, name)
+
+        return None
+
+
+    @classmethod
+    def supportedNetworks(cls):
+        networks = []
+
+        for account in cls.supported:
+            networks.extend(cls.supported[account])
+
+        return networks
+
+
+    @classmethod
+    def hasSupport(cls, network):
+        for account in cls.supported:
+            if network in cls.supported[account]:
+                return True
+
+        return False
+
+
+    @classmethod
+    def networkToAccount(cls, network):
+        for account in cls.supported:
+            if network in cls.supported[account]:
+                return account
+
+        return None
+
+
+    @classmethod
+    def accountToNetworks(cls, account):
+        if account in cls.supported.keys():
+            return cls.supported[account]
+
+        return None
 
 
 class OAuthAccount(Account):
@@ -36,7 +85,7 @@ class OAuthAccount(Account):
 
 
 class UserPassAccount(Account):
-    def __init__(self, network, name, user, password):
+    def __init__(self, network, name, user = None, password = None):
         super(UserPassAccount, self).__init__(network, name)
         self.user = user
         self.password = password
