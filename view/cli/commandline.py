615398840c3a46d2efb0b58709638616dd615130
Juan Jose Salazar Garcia <jjslzgc@gmail.com>
2015-08-08 12:06:29 +0200

    Moved functions from woody.py to commandline.py

--- view/cli/commandline.py
+++ view/cli/commandline.py
@@ -20,5 +20,99 @@
 #  along with Woody.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+import sys
+import atributes
+
+from model.account import Account
+from model.accountmanager import AccountManager
+
 class CommandLine(object):
-    pass
+    def execute(self):
+        command, params = self.processArgs()
+
+        if not command:
+            print __doc__ % sys.argv[0]
+            print 'Use -h --help option to get more details'
+            sys.exit(1)
+
+
+        self.execCommand(command, params)
+
+
+    def processArgs(self):
+        commands = {'CREATE_ACCOUNT' : 2,
+                    'REGISTER_ACCOUNT' : 2,
+                    'DELETE_ACCOUNT' : 2,
+                    'LIST_ACCOUNTS' : 0,
+                    'HELP' : 0}
+        command = None
+        params = []
+
+        if len(sys.argv) < 2:
+            return (None, None)
+
+        for arg in sys.argv[1:]:
+            if arg[0] == '-':
+                if command:
+                    return (None, None)
+                if arg == '-c' or arg == '--create-account':
+                    command = 'CREATE_ACCOUNT'
+                elif arg == '-r' or arg == '--register-account':
+                    command = 'REGISTER_ACCOUNT'
+                elif arg == '-d' or arg == '--delete-account':
+                    command = 'DELETE_ACCOUNT'
+                elif arg == '-l' or arg == '--list-accounts':
+                    command = 'LIST_ACCOUNTS'
+                elif arg == '-h' or arg == '--help':
+                    command = 'HELP'
+                else:
+                    return (None, None)
+            elif command:
+                params.append(arg)
+            else:
+                return (None, None)
+
+        if len(params) != commands[command]:
+            return (None, None)
+
+        return (command, params)
+
+
+    def execCommand(self, cmd, args):
+        am = AccountManager()
+
+        if cmd == 'CREATE_ACCOUNT':
+            account = Account.getAccount(args[0], args[1])
+            if account and am.create(account):
+                print 'Created {0} account {1}'.format(account.network, account.name)
+            else:
+                print 'There was an error during creation'
+        elif cmd == 'DELETE_ACCOUNT':
+            account = Account.getAccount(args[0], args[1])
+            if account and am.remove(account):
+                print 'Removed {0} account {1}'.format(account.network, account.name)
+            else:
+                print 'There was an error during elimination'
+        elif cmd == 'LIST_ACCOUNTS':
+            for account in am.getAll():
+                print '{0} account {1}'.format(account.network, account.name)
+        elif cmd == 'HELP':
+            self.help()
+
+
+    def register(self, url = None):
+        if url:
+            print(url)
+
+        return raw_input('Verifier:')
+
+
+    def help(self):
+        print '{0} {1} - {2}'.format(atributes.APPLICATION_NAME, atributes.APPLICATION_VERSION, atributes.APPLICATION_DESC)
+        print '\n\tUsage: {0} <command> [param ...]'.format(sys.argv[0])
+        print '\nCOMMANDS'
+        print '\n\t -c --create-account <network> <name>\tCreate a new social network account'
+        print '\n\t -r --register-account <network> <name>\t\tRegister an existing account'
+        print '\n\t -d --delete-account <network> <name>\t\tDelete an existing account'
+        print '\n\t -l --list-accounts\t\t\tList all accounts'
+        print '\n\t -h --help\t\t\t\tShow this help message\n'
--- woody.py
+++ woody.py
@@ -22,115 +22,20 @@
 
 "Usage: %s <command> [param ...]"
 
-import sys
-import atributes
-
 from persistence.persistencemanager import PersistenceManager
-from model.account import Account
-from model.account import OAuthAccount
-from model.accountmanager import AccountManager
-from net.twittercontroller import TwitterController
+from view.cli.commandline import CommandLine
 
 def main():
-    command, params = processArgs()
-
-    if not command:
-        print __doc__ % sys.argv[0]
-        print 'Use -h --help option to get more details'
-        sys.exit(1)
-
     initApp()
-    execCommand(command, params)
+
+    cli = CommandLine()
+    cli.execute()
 
 
 def initApp():
     pm = PersistenceManager()
     if not pm.existsConfig():
         pm.createConfig()
-
-
-def processArgs():
-    commands = {'CREATE_ACCOUNT' : 2,
-                'REGISTER_ACCOUNT' : 2,
-                'DELETE_ACCOUNT' : 2,
-                'LIST_ACCOUNTS' : 0,
-                'HELP' : 0}
-    command = None
-    params = []
-
-    if len(sys.argv) < 2:
-        return (None, None)
-
-    for arg in sys.argv[1:]:
-        if arg[0] == '-':
-            if command:
-                return (None, None)
-            if arg == '-c' or arg == '--create-account':
-                command = 'CREATE_ACCOUNT'
-            elif arg == '-r' or arg == '--register-account':
-                command = 'REGISTER_ACCOUNT'
-            elif arg == '-d' or arg == '--delete-account':
-                command = 'DELETE_ACCOUNT'
-            elif arg == '-l' or arg == '--list-accounts':
-                command = 'LIST_ACCOUNTS'
-            elif arg == '-h' or arg == '--help':
-                command = 'HELP'
-            else:
-                return (None, None)
-        elif command:
-            params.append(arg)
-        else:
-            return (None, None)
-
-    if len(params) != commands[command]:
-        return (None, None)
-
-    return (command, params)
-
-
-def execCommand(cmd, args):
-    am = AccountManager()
-
-    if cmd == 'CREATE_ACCOUNT':
-        account = Account.getAccount(args[0], args[1])
-        if account and am.create(account):
-            print 'Created {0} account {1}'.format(account.network, account.name)
-        else:
-            print 'There was an error during creation'
-    elif cmd == 'DELETE_ACCOUNT':
-        account = Account.getAccount(args[0], args[1])
-        if account and am.remove(account):
-            print 'Removed {0} account {1}'.format(account.network, account.name)
-        else:
-            print 'There was an error during elimination'
-    elif cmd == 'LIST_ACCOUNTS':
-        for account in am.getAll():
-            print '{0} account {1}'.format(account.network, account.name)
-    elif cmd == 'HELP':
-        help()
-
-
-def listAccounts():
-    pm = PersistenceManager()
-    accounts = pm.readConfig()['accounts']
-    print accounts
-
-
-def help():
-    print '{0} {1} - {2}'.format(atributes.APPLICATION_NAME, atributes.APPLICATION_VERSION, atributes.APPLICATION_DESC)
-    print '\n\t' + __doc__ % sys.argv[0]
-    print '\nCOMMANDS'
-    print '\n\t -c --create-account <network> <name>\tCreate a new social network account'
-    print '\n\t -r --register-account <network> <name>\t\tRegister an existing account'
-    print '\n\t -d --delete-account <network> <name>\t\tDelete an existing account'
-    print '\n\t -l --list-accounts\t\t\tList all accounts'
-    print '\n\t -h --help\t\t\t\tShow this help message\n'
-
-
-def register(url = None):
-    if url:
-        print(url)
-    return raw_input('Verifier:')
 
 
 if __name__ == '__main__':
